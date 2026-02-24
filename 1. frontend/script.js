6707d5d4547451d095ca4ee1b78b146a969e5b30
khushalp1729-svg <khushalp1729@gmail.com>
2026-02-24 15:37:59 +0530

    Deployment ready

--- 1. frontend/script.js
+++ 1. frontend/script.js
@@ -16,7 +16,7 @@ form.addEventListener("submit", async function (e) {
 
     try {
 
-        const res = await fetch("http://localhost:5000/register", {
+        const res = await fetch(" https://khushal-312b.onrender.com/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
